babcae2a73a3ff6f112457d00b4ee5c4b9de27cd
danurifai <rifaidanu12@gmail.com>
2026-05-04 16:00:42 +0700

    skeleton

---
+++ main.py
@@ -0,0 +1,17 @@
+from dokter import Dokter
+from hewan import Hewan
+from pemilik import Pemilik
+from ruang import Ruangan
+
+class Sistem:
+    def __init__(self, dokter, hewan, pemilik, ruangan):
+        pass
+
+    def hitung_total_tarif():
+        pass
+
+    def tampilkan_info_lengkap():
+        pass
+
+if __name__ == "__main__":
+    Sistem()
